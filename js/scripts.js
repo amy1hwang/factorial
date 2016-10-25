59314f1bf710db5908e75da42e922fc9bd6b48d4
Amy Hwang and Chris Clifford <amy@>
2016-10-25 15:39:54 -0700

    add palindrome detector

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+//Buisness Logic
+
+//Factorial function non-recursive
 var factorial = function(num) {
 	if (num < 0) {
   return "choose a number >= 0"
@@ -12,6 +15,8 @@ var factorial = function(num) {
   }
 }
 
+
+//Recursive factorial function
 var factorialRecursive = function(num) {
   if (num < 0) {
   return "choose a number >= 0"
@@ -23,10 +28,38 @@ var factorialRecursive = function(num) {
   }
 }
 
+//Function to determine if a string is a palindrome.
+var isPalindrome = function(string) {
+  var originalArray = string.split("");
+  var reverseArray = originalArray.slice().reverse();
+  var isPal;
+
+    for (var index = 0; index < originalArray.length; index ++) {
+      if (originalArray[index] === reverseArray[index]) {
+        isPal = true;
+      } else  {
+        isPal = false;
+        break
+      }
+    }
+    if (isPal === true) {
+      return "This is a palindrome!"
+    } else {
+      return "this is NOT a palindrome!"
+    }
+}
+
+
+//Interface Logic
 $(document).ready(function() {
   $("#factorial").submit(function(event){
     event.preventDefault();
     $(".output").text(factorialRecursive(parseInt($("#input").val())));
+
+  })
+  $("#palindrome").submit(function(event){
+    event.preventDefault();
+    $(".output-palindrome").text(isPalindrome($("#input-pal").val()));
 
   })
 })
